c20b0e698bd1eba671f2fd093ad5d53b13cffa03
Dylan Fontaine <dylanfontaine@live.ca>
2022-11-15 19:04:38 -0800

    feat: start building out neovim config from scratch in lua

---
+++ lua/dylf/init.lua
@@ -0,0 +1,8 @@
+if vim.g.vscode then
+    -- Add any vscode specific config
+else
+    -- Neovim specific plugins
+    require("dylf.packer")
+end
+
+require("dylf.set")
---
+++ lua/dylf/set.lua
@@ -0,0 +1,15 @@
+-- vim.opt.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.showtabline = 2
+vim.opt.incsearch = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.mouse = "a"
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
