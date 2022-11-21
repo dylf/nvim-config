21bf0bd63ed19e41e0e99baba57db94bae837e7d
Dylan Fontaine <dylanfontaine@live.ca>
2022-11-20 20:14:29 -0800

    feat: make colorscheme fail gracefully

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,7 +1,16 @@
 require('nightfox').setup({
   options = {
-    transparent = true
+    -- transparent = true
   }
 })
 
-vim.cmd('colorscheme carbonfox')
+
+local colorscheme = 'carbonfox'
+
+local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
+
+if not status_ok then
+  vim.notify('colorscheme ' .. colorscheme .. ' not installed!')
+  return
+end
+
