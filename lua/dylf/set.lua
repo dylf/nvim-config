3bf4554a34e9f4a69bdd318b3985457ec26382cd
Dylan Fontaine <dylanfontaine@live.ca>
2022-11-20 20:17:04 -0800

    feat: add some vim settings from old config i like

--- lua/dylf/set.lua
+++ lua/dylf/set.lua
@@ -1,15 +1,23 @@
--- vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.showtabline = 2
-vim.opt.incsearch = true
 vim.opt.smartindent = true
+vim.opt.smarttab = true
+
+vim.opt.incsearch = true
 vim.opt.wrap = false
 vim.opt.mouse = "a"
 vim.opt.colorcolumn = "80"
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.termguicolors = true
 
 vim.g.mapleader = " "
