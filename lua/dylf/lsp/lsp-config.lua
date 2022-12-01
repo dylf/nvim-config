7cc4b4376fc8f17b3fc1dd3acf2fd1e10adcf960
Dylan Fontaine <dylanfontaine@live.ca>
2022-12-01 08:53:58 -0800

    feat: update some lsp stuff

--- lua/dylf/lsp/lsp-config.lua
+++ lua/dylf/lsp/lsp-config.lua
@@ -1,15 +1,28 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require'lspconfig'.tsserver.setup {
   capabilities = capabilities,
-  on_attach = function()
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
-    vim.keymap.set('n', 'gT', vim.lsp.buf.type_definition, { buffer = 0 })
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = 0 })
-    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buffer = 0 })
-    vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { buffer = 0 })
-    vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { buffer = 0 })
-    vim.keymap.set('n', '<leader>.', vim.lsp.buf.code_action, { buffer = 0 })
+  on_attach = function(_, bufnr)
+
+    -- Helper for remapping
+    local nmap = function(keys, func, desc)
+      if desc then
+        desc = 'LSP: ' .. desc
+      end
+
+      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('gd', vim.lsp.buf.definition, '[g]oto [d]efinition')
+    nmap('gT', vim.lsp.buf.type_definition, '[g]oto [T]ype definition')
+    nmap('gi', vim.lsp.buf.implementation,'[g]oto [i]mplementation') 
+    nmap('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame')
+    nmap('<leader>dn', vim.diagnostic.goto_next, '[d]iagnostic [n]ext')
+    nmap('<leader>dp', vim.diagnostic.goto_prev, '[d]iagnostic [p]rev')
+    nmap('<leader>.', vim.lsp.buf.code_action, 'Code actions')
+    nmap('gr', require('telescope.builtin').lsp_references, '[g]oto [r]eferences')
+
+    -- Autoformat on buf write
     vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
   end,
 }
