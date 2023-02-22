d15c3c9bb3cb0bc941a48468e8e8f31f17598ab2
Dylan Fontaine <dylanfontaine@live.ca>
2023-02-21 22:38:34 -0800

    feat: add yank highlights

--- lua/dylf/init.lua
+++ lua/dylf/init.lua
@@ -14,3 +14,15 @@ end
 
 require('dylf.set')
 require('dylf.remap')
+
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('HighlightYank', {}),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 50,
+    })
+  end,
+})
