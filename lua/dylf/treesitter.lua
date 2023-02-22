13ebebd22be0d68422a85694d2494635de1a43ee
Dylan Fontaine <dylanfontaine@live.ca>
2023-02-21 23:14:11 -0800

    feat: better treesitter keymaps

--- lua/dylf/treesitter.lua
+++ lua/dylf/treesitter.lua
@@ -7,10 +7,10 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<c-backspace>',
+      init_selection = '<CR>',
+      scope_incremental = '<CR>',
+      node_incremental = '<TAB>',
+      node_decremental = '<S-TAB>',
     },
   },
   textobjects = {
