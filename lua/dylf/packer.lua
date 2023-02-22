5cdb3dd444821dcfb7a9f78ea75c474cf662a01f
Dylan Fontaine <dylanfontaine@live.ca>
2023-02-21 21:33:14 -0800

    feat: enable yadm in gitsigns

--- lua/dylf/packer.lua
+++ lua/dylf/packer.lua
@@ -81,7 +81,9 @@ return require('packer').startup({ function(use)
   use {
     'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup()
+      require('gitsigns').setup({
+        yadm = { enable = true }
+      })
     end
   }
 
