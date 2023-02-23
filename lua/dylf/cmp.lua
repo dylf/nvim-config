ae8ca744f0ab016eedc8a528481d8ea7a5d57537
Dylan Fontaine <dylanfontaine@live.ca>
2023-02-22 20:30:37 -0800

    chore: update mappings for cmp

--- lua/dylf/cmp.lua
+++ lua/dylf/cmp.lua
@@ -1,6 +1,13 @@
-vim.opt.completeopt={'menu', 'menuone', 'noselect'}
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
-local cmp = require'cmp'
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
   snippet = {
@@ -13,11 +20,30 @@ cmp.setup({
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
+  mapping = {
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
+    ['<C-e>'] = cmp.mapping.abort(),
+  },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'nvim_lua' },
@@ -29,7 +55,6 @@ cmp.setup({
     ghost_text = true
   },
 })
-
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
