59f80d76d63fbfcb47e437aae8c1b9883429d459
Dylan Fontaine <dylanfontaine@live.ca>
2023-03-22 09:19:36 -0700

    chore: update colorscheme back to catppuccin

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,14 +1,14 @@
-require('nightfox').setup({
-  options = {
-    transparent = true
-  }
+require("nightfox").setup({
+	options = {
+		transparent = true,
+	},
 })
 
-local colorscheme = 'carbonfox'
+local colorscheme = "catppuccin"
 
-local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status_ok then
-  vim.notify('colorscheme ' .. colorscheme .. ' not installed!')
-  return
+	vim.notify("colorscheme " .. colorscheme .. " not installed!")
+	return
 end
