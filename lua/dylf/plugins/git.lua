ff7d746a0f189126964bf04f4fc1753f964cd406
Dylan Fontaine <dylanfontaine@live.ca>
2023-03-26 15:12:26 -0700

    chore: update gitsigns

--- lua/dylf/plugins/git.lua
+++ lua/dylf/plugins/git.lua
@@ -1,9 +1,18 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      yadm = { enable = true },
-    },
-  },
-  "kdheepak/lazygit.nvim",
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			yadm = { enable = true },
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
+		},
+	},
+	"kdheepak/lazygit.nvim",
 }
