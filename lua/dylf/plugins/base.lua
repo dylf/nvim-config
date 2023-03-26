4131143d7510098188b98a41b59f8cf823eaf2c9
Dylan Fontaine <dylanfontaine@live.ca>
2023-03-26 14:07:10 -0700

    feat: add alternate-toggler

--- lua/dylf/plugins/base.lua
+++ lua/dylf/plugins/base.lua
@@ -8,5 +8,16 @@ return {
 		config = function()
 			require("colorizer").setup()
 		end,
+		event = "BufReadPost",
+	},
+	{
+		"rmagatti/alternate-toggler",
+		keys = {
+			{
+				"<leader>tb",
+				"<cmd> lua require('alternate-toggler').toggleAlternate()<cr>",
+				desc = "toggle boolean",
+			},
+		},
 	},
 }
