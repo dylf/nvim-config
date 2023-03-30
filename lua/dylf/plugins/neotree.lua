053ed4a708bed61f8a8e5cd2d28108791ea04da8
Dylan Fontaine <dylanfontaine@live.ca>
2023-03-29 18:12:57 -0700

    feat: add ui in neotree for harpooned files

--- lua/dylf/plugins/neotree.lua
+++ lua/dylf/plugins/neotree.lua
@@ -13,6 +13,34 @@ return {
 			filesystem = {
 				follow_current_file = true,
 				hijack_netrw_behavior = "open_default",
+				components = {
+					harpoon_index = function(config, node)
+						-- Check if harpoon is present
+						local ok, mark = pcall(require, "harpoon.mark")
+						if not ok then
+							return {}
+						end
+						local path = node:get_id()
+						local success, index = pcall(mark.get_index_of, path)
+						if success and index and index > 0 then
+							return {
+								text = string.format("  %d ", index),
+								highlight = config.highlight or "NeoTreeDirectoryIcon",
+							}
+						else
+							return {}
+						end
+					end,
+				},
+				renderers = {
+					file = {
+						{ "icon" },
+						{ "name", use_git_status_colors = true },
+						{ "harpoon_index" },
+						{ "diagnostics" },
+						{ "git_status", highlight = "NeoTreeDimText" },
+					},
+				},
 			},
 		})
 	end,
