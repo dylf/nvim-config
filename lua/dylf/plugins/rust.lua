407dbcd41f1506bc0c0a833edb717307baa6dc81
Dylan Fontaine <me@dylf.dev>
2023-04-18 19:30:36 -0700

    add crates to null-ls sources

--- lua/dylf/plugins/rust.lua
+++ lua/dylf/plugins/rust.lua
@@ -1,7 +1,19 @@
 return {
 	"saecki/crates.nvim",
-	dependencies = "nvim-lua/plenary.nvim",
+	dependencies = {
+		"jose-elias-alvarez/null-ls.nvim",
+		"nvim-lua/plenary.nvim",
+	},
 	event = "BufRead Cargo.toml",
 	tag = "v0.3.0",
-	opts = {},
+	config = function()
+		require("null-ls")
+
+		require("crates").setup({
+			null_ls = {
+				enabled = true,
+				name = "crates.nvim",
+			},
+		})
+	end,
 }
