8b8c88ad3fb9cea6fdfc2c331e4765b37a0e9529
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-01-03 19:38:05 -0800

    snippets updates

--- lua/dylf/plugins/autocomplete.lua
+++ lua/dylf/plugins/autocomplete.lua
@@ -6,6 +6,7 @@ return {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-cmdline",
 			"davidsierradz/cmp-conventionalcommits",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
@@ -76,6 +77,23 @@ return {
 					{ name = "cmp_git" },
 					{ name = "conventionalcommits" },
 					{ name = "buffer" },
+				}),
+			})
+
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
 				}),
 			})
 		end,
--- lua/dylf/plugins/snippets.lua
+++ lua/dylf/plugins/snippets.lua
@@ -4,4 +4,7 @@ return {
 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 	-- install jsregexp (optional!).
 	build = "make install_jsregexp",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+	},
 }
