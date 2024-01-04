09b810f047d6c85821b97deee87648b3d580db78
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-01-03 19:57:11 -0800

    add harpoon to status line

--- lua/dylf/plugins/statusline.lua
+++ lua/dylf/plugins/statusline.lua
@@ -6,9 +6,33 @@ return {
 	config = function()
 		local navic = require("nvim-navic")
 		local icons = require("dylf.icons")
+		local harpoon = require("harpoon.mark")
+
+		local function harpoon_component()
+			local total_marks = harpoon.get_length()
+
+			if total_marks == 0 then
+				return ""
+			end
+
+			local current_mark = "—"
+
+			local mark_idx = harpoon.get_current_index()
+			if mark_idx ~= nil then
+				current_mark = tostring(mark_idx)
+			end
+
+			return string.format("󱡅 %s/%d", current_mark, total_marks)
+		end
 
 		require("lualine").setup({
 			sections = {
+				lualine_b = {
+					{ "branch", icon = "" },
+					harpoon_component,
+					"diff",
+					"diagnostics",
+				},
 				lualine_c = {
 					function()
 						return navic.get_location()
