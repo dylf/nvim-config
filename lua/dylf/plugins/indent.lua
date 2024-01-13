7dfe2b1db50a93ecf5992c9e6af0d74d75ae2985
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-01-13 13:14:53 -0800

    update indent config

--- lua/dylf/plugins/indent.lua
+++ lua/dylf/plugins/indent.lua
@@ -1,13 +1,12 @@
 return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
 		event = { "BufReadPost", "BufNewFile" },
 		opts = {
-			-- char = "▏",
-			char = "│",
-			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-			show_trailing_blankline_indent = false,
-			show_current_context = false,
+			indent = { char = "│" },
+			exclude = { filetypes = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" } },
+			scope = { enabled = false },
 		},
 	},
 
