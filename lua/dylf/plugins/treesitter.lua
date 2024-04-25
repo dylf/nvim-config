fb2ec4152bc82f730905155c3024e21b98fa3706
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-04-25 16:53:01 -0700

    add wip snippet for disabling treesitter on yuge files

--- lua/dylf/plugins/treesitter.lua
+++ lua/dylf/plugins/treesitter.lua
@@ -48,7 +48,16 @@ return {
 					"vim",
 					"yaml",
 				},
-				highlight = { enable = true },
+				highlight = {
+					enable = true,
+					-- disable = function(_, buf)
+					-- 	local max_filesize = 100 * 1024 -- 100 KB
+					-- 	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					-- 	if ok and stats and stats.size > max_filesize then
+					-- 		return true
+					-- 	end
+					-- end,
+				},
 				indent = { enable = true },
 				incremental_selection = {
 					enable = true,
