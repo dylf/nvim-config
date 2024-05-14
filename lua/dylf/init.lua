cd2ced2e91d8f696fd94aed35807cec3d1216c9c
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-05-14 10:22:50 -0700

    chore: remove telescope on startup

--- lua/dylf/init.lua
+++ lua/dylf/init.lua
@@ -6,15 +6,6 @@ require("dylf.ui")
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		-- Open telescope on startup if not opening a specific file
-		if vim.bo.filetype == "" or vim.bo.filetype == "netrw" then
-			vim.cmd("silent! lua require('telescope.builtin').find_files()")
-		end
-	end,
-})
-
 require("os")
 
 vim.filetype.add({
