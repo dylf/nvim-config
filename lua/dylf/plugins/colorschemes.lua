cf611753c1fe55d19e4450b62eb188b4244d980d
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-05-14 10:21:58 -0700

    feat: colorscheme updates

--- lua/dylf/plugins/colorschemes.lua
+++ lua/dylf/plugins/colorschemes.lua
@@ -1,50 +1,41 @@
 return {
 	{
 		"EdenEast/nightfox.nvim",
-		lazy = true,
 		priority = 1000,
-		config = function()
-			require("nightfox").setup({
-				options = {
-					transparent = true,
-					styles = {
-						comments = "italic",
-					},
+		opts = {
+			options = {
+				transparent = true,
+				styles = {
+					comments = "italic",
 				},
-			})
-
-			-- vim.cmd([[colorscheme carbonfox]])
-		end,
+			},
+		},
 	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		event = "VeryLazy",
+		priority = 1000,
 	},
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		priority = 1000,
-		lazy = true,
-		config = function()
-			require("rose-pine").setup({
-				disable_background = true,
-				disable_italics = true,
-			})
-			-- vim.cmd([[colorscheme rose-pine]])
-		end,
+		opts = {
+			disable_background = true,
+			disable_italics = true,
+		},
 	},
 	{
 		"uloco/bluloco.nvim",
-		lazy = false,
 		priority = 1000,
 		dependencies = { "rktjmp/lush.nvim" },
-		config = function()
-			require("bluloco").setup({
-				transparent = true,
-				italics = true,
-			})
-			vim.cmd([[colorscheme bluloco]])
-		end,
+		opts = {
+			transparent = true,
+			italics = true,
+		},
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
 	},
 }
